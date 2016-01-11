bd30d56e33f97dbea4c242124265f8899c751c22
Beastie <corypcole@gmail.com>
2016-01-10 22:02:18 -0500

    Wrote custom date to data stream

--- TCP_Server.js
+++ TCP_Server.js
@@ -2,12 +2,22 @@ var net = require('net');
 var bucket = process.argv ; 
 var port = bucket[2]; 
 var date = new Date(); 
-var data = date.toString(); 
+var customDate = []; 
+       var customTime = [];
+       customDate.push(date.getFullYear()); 
+       customDate.push('0'+(date.getMonth() + 1));
+       customDate.push(date.getDate());
+      // console.log(customDate.join('-'));
+       
+       customTime.push(date.getHours());
+       customTime.push(date.getMinutes());
+       var data = customDate.join('-') + ' ' + customTime.join(':') + '\n';
+//var data = date.toString(); 
 //console.log(port);
 var server = net.createServer(function handleSocket(socket){
    socket.write(data, function callback(){
-       var year = date.getFullYear(); 
-       console.log(year);
-   });
+       
+       socket.end(); 
+   })
 }); 
 server.listen(port);

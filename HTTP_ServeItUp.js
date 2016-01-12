fc0b1577c1d4191e7a5eb6f3441bf3842eecda14
Beastie <corypcole@gmail.com>
2016-01-12 11:46:42 -0500

    better understanding of whats going on now. program is just accepting a request, we receive the path and need to convert to readable stream

--- HTTP_ServeItUp.js
+++ HTTP_ServeItUp.js
@@ -2,15 +2,15 @@ var http = require('http');
 const fs = require('fs'); 
 //listen on this port
 var port = process.argv[2];
-var req = process.argv[3];
+var path = process.argv[3];
 
 //stream response to client with fs.createReadStream() 
 //console.log(req); 
 
 var server = http.createServer(function serve(req,res){
-    console.log(req.method); 
-    
-    
+    console.log(req.rawHeaders); 
+    var src = fs.createReadStream(path); 
+    console.log(src); 
 }); 
 
 server.listen(port); 

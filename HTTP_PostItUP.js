bd8f2e0872e31990c55fae959794448b989c6c2b
Beastie <corypcole@gmail.com>
2016-01-13 09:02:42 -0500

    added counter to determine how many POST request are received by the server

--- HTTP_PostItUP.js
+++ HTTP_PostItUP.js
@@ -1,13 +1,20 @@
 var http = require('http'); 
 var port = process.argv[2]; 
-
+//var collection = []; 
+var count = 0 ; 
 var server = http.createServer(function serve (req, res){ 
     if ( req.method == 'POST'){
+          count++ 
+        
           req.on('data', function getChunks(chunk){
-        
-         var upperCaseChunk = chunk.toString().toUpperCase(); 
-          res.end(upperCaseChunk);
-        
+       
+         //var upperCaseChunk = chunk.toString().toUpperCase(); 
+        // collection.push(chunk); 
+         var transform = chunk.toString().toUpperCase(); 
+         // console.log(collection); 
+         res.end(transform); 
+          
+           console.log(count)
         })
        
         //console.log(req.method); 

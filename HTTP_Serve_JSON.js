f9db6ab6a6bb76b2a1283a114ff7c52ec0eea47b
Beastie <corypcole@gmail.com>
2016-01-14 14:34:45 -0500

    added port

--- HTTP_Serve_JSON.js
+++ HTTP_Serve_JSON.js
@@ -1,10 +1,13 @@
 var http = require('http'); 
 var url = require('url');
+var port = process.argv[2]; 
 
 var server = http.createServer(function serve(req, res){
     var parsedUrl = url.parse(req.url, true);
     var val = parsedUrl.query.iso ;
     
+    res = val; 
     console.log(val);
 });
 
+server.listen(port); 

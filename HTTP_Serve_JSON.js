3d15474fb9c1fe3e4932dd9b0d426cc8c70b2b5d
Beastie <corypcole@gmail.com>
2016-01-14 22:12:59 -0500

    parsed url for key value. Assigned date value to date object

--- HTTP_Serve_JSON.js
+++ HTTP_Serve_JSON.js
@@ -4,10 +4,12 @@ var port = process.argv[2];
 
 var server = http.createServer(function serve(req, res){
     var parsedUrl = url.parse(req.url, true);
-    var val = parsedUrl.query.iso ;
+    var val = parsedUrl.query ;
+    var date = new Date(); 
+    date = val.iso; 
+   // res = val; 
+    console.log(date);
     
-    res = val; 
-    console.log(val);
 });
 
 server.listen(port); 

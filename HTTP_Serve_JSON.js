69889cc7a0621928d205a00250da791a857536a0
Beastie <corypcole@gmail.com>
2016-01-15 13:32:10 -0500

    added conditional logic to 'route' url request

--- HTTP_Serve_JSON.js
+++ HTTP_Serve_JSON.js
@@ -3,19 +3,29 @@ var url = require('url');
 var port = process.argv[2]; 
 
 var server = http.createServer(function serve(req, res){
+  
     var parsedUrl = url.parse(req.url, true);
     var val = parsedUrl.query.iso ;
     var date = new Date(val); 
-
-  var pack =  JSON.stringify(
-      {
-       "hour": date.getHours(),
-       "minute": date.getMinutes(),
-       "seconds": date.getSeconds()
-      }
+   // console.log(parsedUrl.pathname);
+    
+    if (parsedUrl.pathname == '/api/parsetime' ){
+    var pack =  JSON.stringify(
+            {
+            "hour": date.getHours(),
+            "minute": date.getMinutes(),
+            "seconds": date.getSeconds()
+            }
     );
-   // res = val; 
-    console.log(pack);
+    
+    } else { var pack = JSON.stringify(
+        {
+        "unixtime": date.getMilliseconds() 
+        }
+    )}
+    //console.log(pack); 
+    res.write(pack); 
+   res.end(); 
     
 });
 
